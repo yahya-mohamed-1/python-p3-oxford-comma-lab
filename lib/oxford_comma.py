285864fe365c044c41fa73b4ab8ea1d880e3948d
Yahya-Mohamed-1 <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-11-04 08:25:36 +0300

    Lab submission

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if not items:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
